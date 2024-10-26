307616c883147403d884ccd0e79ec19f127f7083
Chinigantayat <Chinmayigantayat08@gmail.com>
2024-10-26 09:43:26 +0530

    Create addition.c
    Addition of two numbers using "scanf",that is used to give  user input  . Int takes 4 bytes space.

---
+++ addition.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main () {
+int x ;
+int y ;
+int sum ;
+printf("Enter the value of x :\n");
+scanf("%d",&x);
+printf("Enter the value of y :\n");
+scanf("%d",&y);
+sum= x+y ;
+printf("Result :%d ",sum );
+return 0;
+}
